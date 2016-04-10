5909f52c26f09a86404639bdf3fe6e00b4cc9529
HarshShah007 <harsh.shah.jayesh@gmail.com>
2016-04-10 22:18:21 +0530

    Changed Register UI

--- register.php
+++ register.php
@@ -2,47 +2,64 @@
 	require('includes/config.php');
 	require('includes/login_db.php');
 	$obj->register('login.php');
+	
 ?>
+<html lang="en">
 
-<html>
 <head>
-	<title>Registration Form</title>
+	<meta charset="utf-8">
+	<title>Register</title>
+
+	<!-- Google Fonts -->
+	<link href='../css/google_roboto_slab.css' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Custom Stylesheet -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<script src="../ajax/jquery.min.js"></script>
 </head>
 
 <body>
-	<h1>Register</h1>
-	<form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
-		<table>
-		<tr>
-			<td>Roll Number (Username): </td>
-			<td><input type="text" name="rollno" /></td>
-		</tr>
-		<tr>
-			<td>Password: </td>
-			<td><input type="password" name="password" /></td>
-		</tr>
-		<tr>
-			<td>Confirm Password: </td>
-			<td><input type="password" name="cpassword" /></td>
-		</tr>
-		<tr>
-			<td>First Name: </td>
-			<td><input type="text" name="fname" value=""/></td>
-		</tr>
-		<tr>
-			<td>Last Name: </td>
-			<td><input type="text" name="lname" value="" /></td>
-		</tr>
-		<tr>
-			<td>Sex: </td>
-			<td>
-				<input type="radio" name="sex" value="Male" checked />Male&nbsp<input type="radio" name="sex" value="Female" />Female
-			</td>
-		</tr>
-		<tr>
-			<td>Date of birth: </td>
-			<td>
-				<select name="date_dob">
+	<div class="container">
+		<div class="top">
+			
+			<h1 id="title" class="hidden">Registration Form</h1>
+		</div>
+		<div class="login-box animated fadeInUp">
+			<div class="box-header">
+				<h2>Registration</h2>
+			</div>
+			
+			<form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
+			
+			<label for="rollno">Roll Number (Username)</label>
+			<br/>
+			<input type="text" id="rollno" name="rollno">
+			<br/>
+			<label for="password">Password</label>
+			<br/>
+			<input type="password" id="password" name="password">
+			<br/>
+			<label for="cpassword">Confirm Password</label>
+			<br/>
+			<input type="password" id="cpassword" name="cpassword">
+			<br/>
+			<label for="fname">First Name</label>
+			<br/>
+			<input type="text" id="fname" name="fname">
+			<br/>
+			<label for="lname">Last Name</label>
+			<br/>
+			<input type="text" id="lname" name="lname">
+			<br/>
+			<label for="sex">Gender</label>
+			<br/>
+			<input type="radio" name="sex" id="sex" value="Male" checked>Male&nbsp<input type="radio" name="sex" value="Female">Female
+			<br/>
+			<label for="date_dob">Date of Birth</label>
+			<br/>
+			<select name="date_dob">
 					<option value="null">DD</option>
 					<?php
 						for($i=1; $i<=31; $i++) {
@@ -50,100 +67,126 @@
 						}
 					?>
 				}
-				</select>
+			</select>
 				&nbsp
-				<select name="month_dob">
+			<select name="month_dob">
 					<option value="null">MM</option>
 					<?php
 						for($i=1; $i<=12; $i++) {
 							printf("<option value=\"%02u\"> %02u </option>",$i, $i);
 						}
 					?>
-				</select>
+			</select>
 				&nbsp
-				<select name="year_dob">
+			<select name="year_dob">
 					<option value="null">YYYY</option>
 					<?php
 						for($i=2006; $i>=1985; $i--) {
 							echo "<option value=\"$i\" > $i </option>";
 						}
 					?>
-				</select>
-			</td>
-		</tr>
-		<tr>
-			<td>Father's Name: </td>
-			<td><input type="text" name="father"/></td>
-		</tr>
-		<tr>
-			<td>Mother's Name: </td>
-			<td><input type="text" name="mother"/></td>
-		</tr>
-		<tr>
-			<td>Category: </td>
-			<td><select name="category">
+			</select>
+			<br/>
+			<br/>
+
+			<label for="father">Father's Name</label>
+			<br/>
+			<input type="text" name="father" id="father"/>
+			<br/>
+
+			<label for="mother">Mother's Name</label>
+			<br/>
+			<input type="text" name="mother" id="mother"/>
+			<br/>
+			<label for="category">Category</label>
+			<br/>
+			<select name="category" id="category">
 				<option value="General">General</option>
 				<option value="SC">SC</option>
 				<option value="ST">ST</option>
 				<option value="OBC-NCL">OBC-NCL</option>
-			</select></td>
-		</tr>
-		<tr>
-			<td>Address: </td>
-			<td><input type="text" name="address_1" /></td>
-		</tr>
-		<tr>
-			<td></td>
-			<td><input type="text" name="address_2" /></td>
-		</tr>
-		<tr>
-			<td></td>
-			<td><input type="text" name="address_3" /></td>
-		</tr>
-		<tr>
-			<td>Department: </td>
-			<td>
-				<input type="radio" name="department" value="I.T" checked />Information Technology&nbsp&nbsp&nbsp<input type="radio" name="department" value="E.C.E" />Electronics and Communication
-			</td>
-		</tr>
-		<tr>
-			<td>Course: </td>
-			<td><select name="course">
+			</select>
+			<br/>
+			<br/>
+			
+			<label for="address">Address</label>
+			<br/>
+			<input type="text" id="address" name="address_1">
+			<br/>
+			<input type="text" id="address" name="address_2">
+			<br/>
+			<input type="text" id="address" name="address_3">
+			<br/>
+
+			<label for="department">Department</label>
+			<br/>
+			<input type="radio" name="department" value="I.T" checked />Information Technology&nbsp&nbsp&nbsp<input type="radio" name="department" value="E.C.E" />Electronics and Communication
+			<br/>
+
+			<label for="course">Course</label>
+			<br/>
+			<select name="course" id="course">
 				<option value="B.Tech">B.Tech</option>
 				<option value="M.tech">M.Tech</option>
 				<option value="B.Tech-M.Tech Dual">B.Tech-M.Tech Dual</option>
 				<option value="P.h.d">P.h.d</option>
-			</select></td>
-		</tr>
-		<tr>
-			<td>Batch: </td>
-			<td><select name="batch">
+			</select>
+			<br/>
+			<br/>
+			
+			<label for="batch">Batch</label>
+			</br>
+			<select name="batch" id="batch">
 				<?php
 					for($i=2016; $i>=2000; $i--) {
 						echo "<option value=\"$i\" > $i </option>";
 					}
 				?>
-			</select></td>
-		</tr>
-		<tr>
-			<td>Semester: </td>
-			<td><select name="semester"><?php 
+			</select>
+			</br>
+<br/>
+			<label for="semester">Semester</label>
+			<br/>
+			<select name="semester" id="semester"><?php 
 				for($i=1; $i<=8; $i++) {
 					echo "<option value=\"$i\" > $i </option>";
 				}
-			?></select></td>
-		</tr>
-		<tr>
-			<td></td>
-			<td><input type="hidden" name="date" value="<?php echo time(); ?>" /></td>
-		<tr>
-		<tr>
-			<td></td>
-			<td><input type="submit" value="Register" /></td>
-		</tr>
-		</table>
-	</form>
-	<p></p>
-	<p>Already registered? <a href="login.php">Log in here</a></p>
+?></select>
+			<br/>
+<br/>
+			<input type="hidden" name="date" value="<?php echo time(); ?>" />
+					
+			<input type="submit" value="Register">
+			<br/>
+			</form>
+			<p>Already a member? <a href="login.php">Login here</a></p>
+			
+		</div>
+		
+		
+	</div>
+	<div id="footer">
+		<h2 style="color: red;"><a href="docs/Abstract.docx">About this project</a></h2>
+	</div>
+
 </body>
+<script>
+	$(document).ready(function () {
+    	$('#logo').addClass('animated fadeInDown');
+    	$("input:text:visible:first").focus();
+	});
+	$('#username').focus(function() {
+		$('label[for="username"]').addClass('selected');
+	});
+	$('#username').blur(function() {
+		$('label[for="username"]').removeClass('selected');
+	});
+	$('#password').focus(function() {
+		$('label[for="password"]').addClass('selected');
+	});
+	$('#password').blur(function() {
+		$('label[for="password"]').removeClass('selected');
+	});
+</script>
+
 </html>
